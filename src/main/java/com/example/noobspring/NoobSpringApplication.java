53690bac2c309c61b408c4de90ae9eda95063ee8
Tae Kyuhyeong <tkbro@linecorp.com>
2022-02-18 11:16:48 +0900

    Initial commit

---
+++ src/main/java/com/example/noobspring/NoobSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.noobspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoobSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoobSpringApplication.class, args);
+	}
+
+}
