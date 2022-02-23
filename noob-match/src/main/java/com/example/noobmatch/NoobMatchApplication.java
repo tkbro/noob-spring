ab81e7cd586e01c9857f43c6ca0bb6eed21c1bd5
Tae Kyuhyeong <tkbro@linecorp.com>
2022-02-23 21:17:02 +0900

    매칭 서버용 프로젝트 추가

---
+++ noob-match/src/main/java/com/example/noobmatch/NoobMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.noobmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoobMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoobMatchApplication.class, args);
+    }
+
+}
---
+++ noob-match/src/test/java/com/example/noobmatch/NoobMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noobmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoobMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
