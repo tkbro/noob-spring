da043e7ed9aa746f4944e22cf3aa2a12ac88653e
Tae Kyuhyeong <tkbro@linecorp.com>
2022-02-23 23:59:12 +0900

    사용안하는 코드 제거

--- noob-match/src/main/java/com/tkbro/noobmatch/server/NoobMatchApplicationContextLoader.java
+++ noob-match/src/main/java/com/tkbro/noobmatch/server/NoobMatchApplicationContextLoader.java
@@ -2,8 +2,6 @@ package com.tkbro.noobmatch.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class NoobMatchApplicationContextLoader {
@@ -11,13 +9,6 @@ public class NoobMatchApplicationContextLoader {
 
     public static void main(String[] args) {
         SpringApplication.run(NoobMatchApplicationContextLoader.class, args);
-       /* try {
-            SpringApplication.run
-            AbstractApplicationContext context = new ClassPathXmlApplicationContext();
-            context.registerShutdownHook();
-        } catch (Exception ex) {
-            System.exit(1);
-        }*/
     }
 }
 
