a26074c8a4209c95a3fbaa739b5739ed3633cd9f
tkbro <wlehoapt@gmail.com>
2022-02-24 23:35:54 +0900

    Server handler logger apply.

--- noob-match/src/main/java/com/tkbro/noobmatch/example/ExampleHandler.java
+++ noob-match/src/main/java/com/tkbro/noobmatch/example/ExampleHandler.java
@@ -3,23 +3,27 @@ package com.tkbro.noobmatch.example;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExampleHandler extends ChannelInboundHandlerAdapter {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public void channelActive(ChannelHandlerContext context) throws Exception {
-        System.out.println("Channel Active.");
+        logger.info("Channel Active.");
     }
 
     @Override
     public void channelRead(ChannelHandlerContext context, Object msg) throws Exception {
-        System.out.println("message: " + msg.toString());
-        context.write("ok");
+        logger.info("message: " + msg.toString());
+        context.write("ok".concat(msg.toString()).concat("\n"));
         context.flush();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext context, Throwable cause) throws Exception {
+        logger.error(cause.toString());
         cause.printStackTrace();
         context.close();
     }
